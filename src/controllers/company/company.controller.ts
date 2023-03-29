b49438a01ab9087a341ace4c314dfb0b46293183
Apple <apple@Apples-MacBook-Pro.local>
2023-03-28 23:38:06 -0700

    Added generic helper function for checking missing form fields

--- src/controllers/company/company.controller.ts
+++ src/controllers/company/company.controller.ts
@@ -1,18 +1,20 @@
 import { Request, Response } from 'express';
 import Companies from '../../models/Company';
 import logger from '../../logger';
+import { findMissingObjectValues } from '../../helpers/missingCredentials.helper';
 
 export const CompanyBasicDetails = async (req: Request, res: Response) => {
     const { companyName, legalAddress, fein, companyCredentials, profileCompletionStep } = req.body;
-    if (!companyName || !legalAddress || !fein || !companyCredentials || !profileCompletionStep) {
+    const missingValue = await findMissingObjectValues ({ companyName, legalAddress, fein, companyCredentials, profileCompletionStep })
+    if (missingValue) {
         logger.log({
             level: 'debug',
-            message: 'Company details missing.',
+            message: missingValue,
             consoleLoggerOptions: { label: 'API' }
           });
           return res.status(422).json({
             success: false,
-            message: 'Company details are Missing'
+            message: missingValue
           });
     }
     const files: any = req?.files;
